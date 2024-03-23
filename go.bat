8920ec8eff57ea84e791bcd4a8d79efd8a068337
Arc <arcsdoor@gmail.com>
2024-03-23 16:30:52 -0400

    edit git

--- go.bat
+++ go.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\RuneMate\jre\bin\java" -jar -Xmx4096m "C:\Program Files\RuneMate\RuneMate.jar" -sdk -u wrengar -p savage
+git pull
+"C:\Program Files\RuneMate\jre\bin\java" -jar -Xmx4096m "C:\Program Files\RuneMate\RuneMate.jar" -sdk -u USER -p PASS
