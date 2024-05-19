bf07b73dd3fc14302165351b90974f9d13f1708b
Arc <arcsdoor@gmail.com>
2024-05-19 04:47:52 -0400

    new build tool implementation

--- go.bat
+++ go.bat
@@ -1,2 +1,2 @@
 git pull
-"C:\Program Files\RuneMate\jre\bin\java" -jar -Xmx4096m "C:\Program Files\RuneMate\RuneMate.jar" -sdk -u USER -p PASS
+"C:\Program Files\RuneMate\jre\bin\java" -jar -Xmx4096m "C:\Program Files\RuneMate\RuneMate.jar" -sdk
